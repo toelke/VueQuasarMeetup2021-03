b3b43a21f5a509d49288e7557d1e969d2cda9876
Philipp Tölke <ptoelke@tecracer.de>
2021-03-16 20:22:25 +0100

    add basic nav

--- code/src/layouts/MainLayout.vue
+++ code/src/layouts/MainLayout.vue
@@ -10,7 +10,20 @@
       </q-toolbar>
     </q-header>
 
-    <q-drawer v-model="leftDrawerOpen" show-if-above bordered content-class="bg-grey-1"> </q-drawer>
+    <q-drawer v-model="leftDrawerOpen" show-if-above bordered content-class="bg-grey-1">
+      <q-list v-for="(menuItem, index) in menuList" :key="index">
+        <q-item :to="localized_url(menuItem.link)" clickable v-ripple>
+          <q-item-section avatar>
+            <q-icon :name="menuItem.icon" />
+          </q-item-section>
+          <q-item-section>
+            {{ menuItem.title }}
+            <q-item-label caption>{{ menuItem.caption }}</q-item-label>
+          </q-item-section>
+        </q-item>
+        <q-separator v-if="menuItem.separator" />
+      </q-list>
+    </q-drawer>
 
     <q-page-container>
       <router-view />
@@ -24,8 +37,39 @@ export default {
   components: {},
   data() {
     return {
-      leftDrawerOpen: false
+      leftDrawerOpen: false,
+      menuList: [
+        {
+          title: 'Upload',
+          caption: 'This is to upload the files',
+          icon: '',
+          link: '/upload'
+        },
+        {
+          title: 'View',
+          caption: '',
+          icon: 'code',
+          link: '/view'
+        },
+        {
+          title: 'Impressum',
+          caption: '',
+          icon: 'play_for_work',
+          link: '/impressum'
+        }
+      ]
     };
+  },
+  methods: {
+    localized_url(url) {
+      return url;
+    },
+    async logout() {
+      const stat = await auth_logout();
+      if (stat.status == 'ok') {
+        this.loggedIn = false;
+      }
+    }
   }
 };
 </script>
